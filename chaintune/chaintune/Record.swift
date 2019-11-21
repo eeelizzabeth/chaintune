fcc25d324c4f6cc6c03478252e9385eebe75f703
Iris Manriquez <irismanriquez@Iriss-MacBook-Air.local>
2019-11-20 18:12:37 -0800

    database pushing mock data

--- chaintune/chaintune/Record.swift
+++ chaintune/chaintune/Record.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+import SwiftUI
+import FirebaseDatabase
+
+struct Record: Identifiable {
+    
+    let ref: DatabaseReference?
+    let key: String
+    let duration: Int
+    let startTime: String
+    let endTime: String
+    let id: String
+    
+    init(duration: Int, startTime: String, endTime: String, key: String = "") {
+        self.ref = nil
+        self.key = key
+        self.duration = 0
+        self.startTime = String()
+        self.endTime = String()
+        self.id = key
+    }
+    init?(snapshot: DataSnapshot) {
+        guard
+            let value = snapshot.value as? [String: AnyObject],
+            let duration = value["duration"] as? Int,
+            let startTime = value["startTime"] as? String,
+            let endTime = value["endTime"] as? String
+        else {
+                return nil
+            }
+        self.ref = snapshot.ref
+        self.key = snapshot.key
+        self.duration = duration
+        self.startTime = startTime
+        self.endTime = endTime
+        self.id = snapshot.key
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "duration": duration,
+            "startTime": startTime,
+            "endTime": endTime,
+        ]
+    }
+}
+
